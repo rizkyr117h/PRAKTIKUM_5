7c8945fa0d75be3ca638402ff0c01047885dbb47
rizkyr117h <84673678+rizkyr117h@users.noreply.github.com>
2022-03-26 11:14:13 +0700

    penambahan design by contract pada semua kelas

--- jurnal5_1302203122/jurnal5_1302203122/Program.cs
+++ jurnal5_1302203122/jurnal5_1302203122/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace jurnal5_1302203122
 {
@@ -17,12 +18,18 @@ namespace jurnal5_1302203122
             SayaTubeVideo video3 = new SayaTubeVideo("Cars 3");
             video3.IncreasePLayCount(30);
             SayaTubeVideo video4 = new SayaTubeVideo("Cars 4");
+/*            for (int i = 0; i < 10; i++)
+            {
+                video3.IncreasePLayCount(int.MaxValue);
+            }*/
             SayaTubeVideo video5 = new SayaTubeVideo("Cars 5");
             SayaTubeVideo video6 = new SayaTubeVideo("Cars 6");
             SayaTubeVideo video7 = new SayaTubeVideo("Cars 7");
             SayaTubeVideo video8 = new SayaTubeVideo("Cars 8");
             SayaTubeVideo video9 = new SayaTubeVideo("Cars 9");
             SayaTubeVideo video10 = new SayaTubeVideo("Cars 10");
+
+/*            SayaTubeUser profile2 = new SayaTubeUser(null);*/
 
 
             SayaTubeUser profile = new SayaTubeUser("Rizky");
@@ -36,7 +43,10 @@ namespace jurnal5_1302203122
             profile.addVideo(video8);
             profile.addVideo(video9);
             profile.addVideo(video10);
+            profile.addVideo(null);
             profile.PrintAllVideoPlayCount();
+
+
 
             
             
@@ -52,7 +62,21 @@ namespace jurnal5_1302203122
         public SayaTubeUser(string nama)
 
         {
-            Username = nama;
+
+            Debug.Assert(nama.Length < 101, "Max 100");
+            Debug.Assert(nama != null, "Jangan Kosong");
+
+            string testing;
+
+            try
+            {
+                testing = checked(nama);
+                Username = nama;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
             uploadedVideos = new List<SayaTubeVideo>();
 
             Random generate = new Random();
@@ -73,7 +97,18 @@ namespace jurnal5_1302203122
 
         public void addVideo(SayaTubeVideo data)
         {
-            uploadedVideos.Add(data);
+            Debug.Assert(data != null, "Data Jangan Kosong");
+
+            SayaTubeVideo testing;
+            try
+            {
+                testing = checked(data);
+                uploadedVideos.Add(data);
+            }catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            
         }
 
         public void PrintAllVideoPlayCount()
@@ -95,7 +130,20 @@ namespace jurnal5_1302203122
 
         public SayaTubeVideo(string judul)
         {
-            title = judul;
+            Debug.Assert(judul != null, "Jangan Kosong");
+            Debug.Assert(judul.Length< 200, "Terlalu Panjang");
+
+            string testing;
+            try
+            {
+                testing = checked(judul);
+                this.title = judul;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
             Random generate = new Random();
             this.id = generate.Next(0, 100000);
             this.playCount = 0;
@@ -104,7 +152,25 @@ namespace jurnal5_1302203122
 
         public void IncreasePLayCount(int angka)
         {
+ 
+            Debug.Assert(angka <= 25000000, "Tidak Boleh Lebih Dari 25.000.000!!!");
+            Debug.Assert(angka > 0, "Jangan Negative");
+            Debug.Assert(angka < int.MaxValue, "Melebihi Batas MAX");
+            int test;
+            try
+            {
+                test = checked(this.playCount + angka);
+                this.playCount += angka;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error : " + ex.Message);
+                playCount = playCount;
+            }
+
             playCount = playCount + angka;
+
+
         }
 
         public void PrintVideoDetails()
